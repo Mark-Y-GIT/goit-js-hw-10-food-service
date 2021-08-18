f83cac4a472ff99bb91245a4dacf97918a40ee16
Mark-Y <yakushyn.mark@gmail.com>
2021-08-19 01:46:51 +0300

    Подправил темы

--- src/js/themeChanger.js
+++ src/js/themeChanger.js
@@ -1,13 +1,14 @@
 import refs from './refs.js';
+import { Theme } from './thems.js';
 
 export default function changeTheme() {
   if (refs.themeSwitchToggleRef.checked) {
-    refs.bodyRef.classList.remove('light-theme');
-    refs.bodyRef.classList.add('dark-theme');
+    refs.bodyRef.classList.remove(Theme.LIGHT);
+    refs.bodyRef.classList.add(Theme.DARK);
     localStorage.setItem('theme', 'dark-theme');
   } else {
-    refs.bodyRef.classList.remove('dark-theme');
-    refs.bodyRef.classList.add('light-theme');
+    refs.bodyRef.classList.remove(Theme.DARK);
+    refs.bodyRef.classList.add(Theme.LIGHT);
     localStorage.setItem('theme', 'light-theme');
     refs.themeSwitchToggleRef.checked = false;
   }
