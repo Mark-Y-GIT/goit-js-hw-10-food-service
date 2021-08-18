a223b09153eef573e45a2e41d96b0abbdde4d4d9
Mark <yakushyn.mark@gmail.com>
2021-08-18 18:46:29 +0300

    подключил тему

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 // import './sass/main.scss';
 import mainTemplate from './templates/template.hbs';
 import mainMarkup from './menu.json';
+import { add } from 'lodash';
 
 const markup = mainTemplate(mainMarkup);
 
 const menuRef = document.querySelector('.js-menu');
 
 menuRef.insertAdjacentHTML('beforeend', markup);
+
+const themeSwitchToggleRef = document.querySelector('.theme-switch__toggle');
+
+const bodyRef = document.querySelector('.js-theme');
+console.log(bodyRef);
+
+themeSwitchToggleRef.addEventListener('change', () => {
+  bodyRef.classList.toggle('dark-theme');
+});
