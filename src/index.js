823e145cf229e9a1dcbbedf3610a0da8245d6209
Mark <yakushyn.mark@gmail.com>
2021-08-18 18:20:27 +0300

    настроил шаблон

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-import './sass/main.scss';
+// import './sass/main.scss';
+import mainTemplate from './templates/template.hbs';
+import mainMarkup from './menu.json';
+
+const markup = mainTemplate(mainMarkup);
+
+const menuRef = document.querySelector('.js-menu');
+
+menuRef.insertAdjacentHTML('beforeend', markup);
